cf2389b63ecc2f9b72a6e69abcd2d7cbf54f9673
Tobias Goldschmidt <220487@vut.cz>
2020-04-02 20:55:43 +0200

    Added comments

--- find_konvex_hull.py
+++ find_konvex_hull.py
@@ -4,6 +4,8 @@ from math import degrees
 
 
 def find_lowest(obstacle):
+    # Funkcia na hladanie najnizsieho, pripadne najnizsieho a najlavejsieho bodu.
+    # Vstup je pole bodov v rovine, kazdy bod je zadany polom v tvare [x,y].
     p0 = obstacle[0]
     for point in obstacle:
         if point[1] < p0[1]:
@@ -14,12 +16,16 @@ def find_lowest(obstacle):
     return p0
 
 
-def distance(point, p0):
-    dist = sqrt((point[0] - p0[0]) ** 2 + (point[1] - p0[1]) ** 2)
+def distance(point1, point2):
+    # Funkcia na najdenie vzdialenosti dvoch bodov v rovine pomocou pytagorovej vety.
+    dist = sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
     return dist
 
 
 def angel_from_p0(point, p0):
+    # Funkcia na najdenie uhlu od pociatocneho bodu.
+    # So stupnami pracujeme pre zjednodusenie podminenokovania.
+    # Je potrebne vymedzit pripady pre ktore funkcia atan nieje definovana resp dava 2 rozne vysledky.
     if point[0] == p0[0]:
         angel = 90
     elif point[1] == p0[1] and point[0] > p0[0]:
@@ -35,6 +41,9 @@ def angel_from_p0(point, p0):
 
 
 def sort_by_angel(obstacle, p0):
+    # Funkcia ktora zoradi pole bodov podla uhlu od pociatocneho bodu.
+    # Vstup je pole bodov, kazdy bod je zadany polom v tvare [x,y], a pociatocny bod, tiez v tvare [x,y].
+    # Ak mame dva body s rovnakym uhlom voci pociatocnemu bodu vylucime ten blizsi k pociatocnemu bodu.
     non_konvex_hull = [p0]
     for point in obstacle:
         arg = True
@@ -60,20 +69,30 @@ def sort_by_angel(obstacle, p0):
 
 
 def vector(a, b):
+    # Funkcia ktora zisti smernicu priamky prechdzajucej dvoma bodmi.
     result = (b[1] - a[1]) / (b[0] - a[0])
     return result
 
 
 def left_right(p, c, n, p0):
+    # Funkcia ktora zistuje ci lezi bod vpravo alebo vlavo od spojnice dvoch bodov
+    # na zaklade premietnutia suradnic bodu do smernice priamky prechadzajucej tymito bodmi.
     result = False
-    x = [n[0] + (c[1] - n[1])/vector(n, p), c[1]]
-    y = [c[0], n[1] + (c[0] - n[0])*vector(n, p)]
+    x = [n[0] + (c[1] - n[1]) / vector(n, p), c[1]]
+    y = [c[0], n[1] + (c[0] - n[0]) * vector(n, p)]
     if (distance(c, p0) > distance(x, p0)) or (distance(c, p0) > distance(y, p0)):
         result = True
     return result
 
 
 def find_konvex_hull(non_konvex):
+    # Riadica funkcia tohot skriptu.
+    # Vrati nekonvexny plast vsetkych utvarov.
+    # Prebieha aj kontrola ci je mozne vytvorit konvexny obal utvaru.
+    # Najskor najdeme najnizssi bod kazdeho utvaru, potom zoradime body podla uhlov voci nemu,
+    # potom prechdzame vsetky body ci zvieraju konvexny uhol s predchzajucimi dvoma bodmi, ak nie,
+    # vyradime posledny bod ktory sme zaradili do konvexneho plasta a opakujeme az kym nezviera
+    # bod s dvoma poslendymi bodmi konvexny uhol.
     all_konvex_hulls = []
     for obstacle in non_konvex:
         konvex_hull = []
