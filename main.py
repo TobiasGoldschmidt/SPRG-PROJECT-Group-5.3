5db6d4f17a01748efcf1e2a43252c0eaab5961b5
TomasKriz <62022452+TomasKriz@users.noreply.github.com>
2020-04-09 15:54:11 +0200

    Added function for executing all given routes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-﻿import json
-import find_konvex_hull
-import find_route
+import json
+import find_konvex_hull.py
+import find_route.py
 
 
 def get_map_data():
     positions = []
-    for i in json.load(open('map_data_0.json', 'r'))['object']:
+    for i in json.load(open('map_data_1.json', 'r'))['object']:
         coordinates = i['coordinates']
         positions.append(coordinates)
     return positions
@@ -23,21 +23,22 @@ def get_path():
 
 def main():
     check = []
-    route = []
     for item in get_map_data():
         for i in item:
             check.append(i)
     for item in get_map_data():
         for i in item:
             if check.count(i) > item.count(i):
-                print('Není možné pokračovat, jeden bod se vykytuje ve více objektech.')
+                print('Není možné pokračovat, jeden bod sa vyskytuje ve více objektech.')
                 break
-            else:
-                route = find_route.find_route(find_konvex_hull.find_konvex_hull(get_map_data()))
         else:
             continue
         break
-    return route
+    else:
+        plan = find_konvex_hull.find_konvex_hull(get_map_data())
+        for i in range(len(get_path())):
+            print(find_route.find_route(get_path()[i - 1][0], get_path()[i - 1][1], plan))
 
 
-print(main())
+if __name__ == '__main__':
+    main()
